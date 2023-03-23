856e9e7ff0d0fdc3e1ea7e60f45f3abb45fd6222
royboy31 <113940559+royboy31@users.noreply.github.com>
2023-03-24 00:47:44 +0100

    Revert "Update page-servicebooking.php"
    This reverts commit 23fb9420f81d5b59bf5eb3acf2f6ee9062ff1d1d.

--- page-servicebooking.php
+++ page-servicebooking.php
@@ -1,89 +1,4 @@
 <?php /* Template Name: Page_service_boking */ ?>
-<!--Load Preloader-->
-
-<div id="preloader">
-  <h4 id="loading-text">LiveLND is loading for you
-    <span class="dot-container">
-      <span class="dot-animation">.</span>
-      <span class="dot-animation">.</span>
-      <span class="dot-animation">.</span>
-      <span class="dot-animation">.</span>
-    </span>
-  </h4>
-</div>
-
-<script>
-document.body.classList.add("preloader-active");	
-</script>
-<style>
-body.preloader-active {
-  overflow: hidden;
-}
-
-#preloader {
-  display: flex; /* Add flexbox to the preloader */
-  align-items: center; /* Center the loading text vertically */
-  justify-content: center; /* Center the loading text horizontally */
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: black;
-  z-index: 9999;
-}
-
-#loading-text {
-  font-size: 24px !important;
-  font-weight: bold !important;
-  text-align: center !important;
-  white-space: nowrap;
-  letter-spacing: 0.15em !important;
-  color: #ff8250;
-  position: relative; /* Change position to relative */
-}
-
-.dot-container {
-  display: inline-flex;
-  position: absolute;
-  top: 0;
-  left: 100%;
-  margin-left: 5px;
-  width: 50px; /* Add a fixed width */
-  height: 24px; /* Add a fixed height */
-}
-.dot-animation {
-  position: absolute;
-  animation: dot-bounce 1.2s linear infinite;
-  margin-left: 2px;
-  will-change: transform;
-}
-
-.dot-animation:nth-child(2) {
-  margin-left: 14px;
-  animation-delay: 0.2s;
-}
-
-.dot-animation:nth-child(3) {
-  margin-left: 26px;
-  animation-delay: 0.4s;
-}
-
-.dot-animation:nth-child(4) {
-  margin-left: 38px;
-  animation-delay: 0.6s;
-}
-
-@keyframes dot-bounce {
-  0%, 80%, 100% {
-    transform: translateY(0);
-  }
-  40% {
-    transform: translateY(-10px);
-  }
-}
-
-</style>
 
 <?php
 $server_name = "";
@@ -597,16 +512,6 @@ $worduser = 'user_' . $externalid;
                             </div>
                         </div>
                     </div>
-                    
-                    <!--Unload Preloader-->
-                    <script>
-                    window.addEventListener("load", function(){
-                    var preloader = document.getElementById('preloader');
-                    preloader.style.display = 'none';
-                    document.body.classList.remove("preloader-active");
-                    });
-                    </script>
-
                     <div class="row testisec ">
                         <div class="fulltestirow">
                             <div class="row">
