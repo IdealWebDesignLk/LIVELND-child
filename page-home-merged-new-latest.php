e3ca46c83b0633ee3613230b735637b15ef3a418
royboy31 <113940559+royboy31@users.noreply.github.com>
2023-03-27 23:33:53 +0200

    Update page-home-merged-new-latest.php
    removing cat placeholder

--- page-home-merged-new-latest.php
+++ page-home-merged-new-latest.php
@@ -444,7 +444,7 @@ $videosrc =  $server_name . '/wp-content/uploads/2022/09/pexels-artem-podrez-575
 
                         <label>Category</label>
 
-                        <select name="kd-search-category" id="kd-search-ccategory" onchange="selectResultBasedCategory(event)" placeholder="Category">
+                        <select name="kd-search-category" id="kd-search-ccategory" onchange="selectResultBasedCategory(event)">
                             <option value="select-category">Select Category</option>
                             <?php
 
