62be5ef9a7d51564722562af369717044309a647
royboy31 <113940559+royboy31@users.noreply.github.com>
2023-04-21 18:36:40 +0200

    removing google tag manager from head

--- header.php
+++ header.php
@@ -34,9 +34,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	</head>
 	
 <body <?php body_class(); ?>>
-	<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TWP6JZK"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
 
 <?php hello_elementor_body_open(); ?>
 
