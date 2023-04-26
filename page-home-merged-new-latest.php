dd222dab3daf6748f9aa0d2439daa6298f055d1a
kamindu <kamindu@itone.biz>
2023-04-27 00:01:19 +0530

    changed query again

--- page-home-merged-new-latest.php
+++ page-home-merged-new-latest.php
@@ -795,9 +795,11 @@ $videosrc =  $server_name . '/wp-content/uploads/2022/09/pexels-artem-podrez-575
 
                                         // getting cross sells
                                         global $wpdb;
-                                        $results = $wpdb->get_results("SELECT * FROM  " . $wpdb->prefix . "amelia_services WHERE 'id' = '" . $servicesingleid . "'");
+                                        // $results = $wpdb->get_results("SELECT * FROM  " . $wpdb->prefix . "amelia_services WHERE 'id' = '" . $servicesingleid . "'");
                                         // echo "SELECT `settings` FROM  " . $wpdb->prefix . "amelia_services WHERE 'id' = '" . $servicesingleid . "'";
+                                        $result = $wpdb->get_results("SELECT `settings` FROM " . $wpdb->prefix . "amelia_services WHERE id = '" . $servicesingleid . "'");
                                         echo "SELECT `settings` FROM " . $wpdb->prefix . "amelia_services WHERE id = '" . $servicesingleid . "'";
+                                        
                                         foreach( $results as $result ) {
 
                                             print_r($result);                                    
