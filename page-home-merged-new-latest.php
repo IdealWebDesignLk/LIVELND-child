27b2673b90cbf8029495be20948a2c92df8b2273
kamindu <kamindu@itone.biz>
2023-04-27 10:39:47 +0530

    changed query

--- page-home-merged-new-latest.php
+++ page-home-merged-new-latest.php
@@ -795,8 +795,8 @@ $videosrc =  $server_name . '/wp-content/uploads/2022/09/pexels-artem-podrez-575
 
                                         // getting cross sells
                                         global $wpdb;
-                                        $results = $wpdb->get_results("SELECT `settings` FROM " . $wpdb->prefix . "amelia_services WHERE id = '" . $servicesingleid . "'");
-                                        echo ("SELECT `settings` FROM " . $wpdb->prefix . "amelia_services WHERE id = '" . $servicesingleid . "'");
+                                        $results = $wpdb->get_results("SELECT `settings` FROM " . $wpdb->prefix . "amelia_services WHERE id = " . $servicesingleid . "");
+                                        echo ("SELECT `settings` FROM " . $wpdb->prefix . "amelia_services WHERE id = " . $servicesingleid . "");
                                         print_r($results);
                                 ?>
                                         <div class="kd-popup-content hidden" id="kd-popup-<?php echo $servicesingleid; ?>">
