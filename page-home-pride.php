4c3ef633ac4744c008f8bab8fc9456b39edbdd78
royboy31 <113940559+royboy31@users.noreply.github.com>
2023-04-27 11:07:26 +0200

    Update page-home-pride.php

--- page-home-pride.php
+++ page-home-pride.php
@@ -91,7 +91,7 @@ get_header();
 }
 
     #loading-text {
-        font-size: 44px !important;
+        font-size: 34px !important;
         font-weight: 700 !important;
         text-align: center !important;
         white-space: nowrap;
