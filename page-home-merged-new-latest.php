50fbdee0b4e747b52e8dfe3f89f5a0b5ac80b2eb
kamindu <kamindu@itone.biz>
2023-04-27 12:45:29 +0530

    test

--- page-home-merged-new-latest.php
+++ page-home-merged-new-latest.php
@@ -795,6 +795,8 @@ $videosrc =  $server_name . '/wp-content/uploads/2022/09/pexels-artem-podrez-575
 
                                         // getting cross sells
                                         // global $wpdb;
+
+                                        print_r($wpdb);
                                         $kdresults = $wpdb->get_results("SELECT `settings` FROM wp_821991_amelia_services WHERE id = 366");
                                         echo ("SELECT `settings` FROM " . $wpdb->prefix . "amelia_services WHERE id = " . $servicesingleid . "");
                                         print_r($kdresults[0]);
