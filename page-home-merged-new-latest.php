dd5b3287ac977ce889ce2f1515c1a552f3c7b2b8
kamindu <kamindu@itone.biz>
2023-04-27 13:13:34 +0530

    echo formatting

--- page-home-merged-new-latest.php
+++ page-home-merged-new-latest.php
@@ -806,9 +806,10 @@ $videosrc =  $server_name . '/wp-content/uploads/2022/09/pexels-artem-podrez-575
                                         
                                         // print_r($kdresults[0]);
                                         foreach ($kdresults as $rst) {
+                                            echo '<pre>';
                                             // print_r($rst);
                                             echo $rst->settings;
-                                            echo '<pre>';
+                                            echo '</pre>';
                                         }
                                 ?>
                                         <div class="kd-popup-content hidden" id="kd-popup-<?php echo $servicesingleid; ?>">
