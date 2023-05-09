c199d473073125b806e100d449fc013c39a4d838
kamindu <kamindu@itone.biz>
2023-05-09 15:58:39 +0530

    adding new content

--- page-lp-pride.php
+++ page-lp-pride.php
@@ -6,7 +6,7 @@ get_header();
 <!--Load Preloader-->
 
 <div id="preloader">
-    <div class="color-container">
+    <!-- <div class="color-container">
         <div class="color red"></div>
         <div class="color orange"></div>
         <div class="color yellow"></div>
@@ -14,7 +14,7 @@ get_header();
         <div class="color blue"></div>
         <div class="color indigo"></div>
         <div class="color violet"></div>
-    </div>
+    </div> -->
     <h4 id="loading-text">Live L&D is loading the Pride page
     <span class="dot-container">
             <span class="dot-animation">.</span>
@@ -479,27 +479,26 @@ $videosrc =  $server_name . '/wp-content/uploads/2022/09/pexels-artem-podrez-575
                                 <div class="container homebodysec">
                                     <div class="innerhomesecondrow">
                                         <div class="secondsec">
-                                            <h1>Book a Virtual Live Speaker for <span class="pridetitle">Pride 2023!</span></h1>
+                                            <h1>Book an expert speaker to celebrate <span class="pridetitle">Pride Month!</span></h1>
 
                                             <div class="row textrowdiv">
                                                 <div class="col-md-4">
                                                     <img class="threeblurbimg" src="<?php echo $server_name . "/wp-content/uploads/2023/04/Group-10-white.png" ?>">
                                                     <h4>1. Find your virtual speaker</h4>
-                                                    Our curators have handpicked global experts who speak about DEI, Mental Wellbeing, Leadership and many more topics.
-                                                    Discover them by hovering over their talks.
+                                                    Our curators have handpicked global experts who speak about Pride, LGBTQ rights, biases, inclusion and tens of other topics. Discover speakers by hovering over their talks.
                                                 </div>
 
                                                 <div class="col-md-4">
                                                     <img class="threeblurbimg" src="<?php echo $server_name . "/wp-content/uploads/2023/04/Group-11-white.png" ?>">
-                                                    <h4>2. Check availability</h4>
-                                                    All Live L&D speakers have connected their calendar to our site. Check their availability via the search form bellow.
+                                                    <h4>2. Check availability and fees</h4>
+                                                    All Live L&D speakers have connected their calendar to our platform. Search for speakers based on availability and budget. Or schedule a Meet & Greet with the expert before booking.
                                                 </div>
                                                 <div class="col-md-4">
                                                     <img class="threeblurbimg" src="<?php echo $server_name . "/wp-content/uploads/2023/04/Group-12-white.png" ?>">
                                                     <h4>3. Hold Time Slot / Book Instantly</h4>
-                                                    Pick a day/time slot for your virtual talk. We will hold that time slot for 72 hours for you and contact you to answer your questions.
+                                                    Pick a day/time slot for your Pride Month Talk. Book immediately and pay later. Or pick a time slot for the speaker to hold for 72 hours and we will contact you instantly to answer all your questions before you make an actual booking.
                                                 </div>
-                                                <p class="bottomtext">Sessions will be held in your company’s own Microsoft Teams, Google Meet, Zoom or Webex environment.</p>
+                                                <p class="bottomtext"> <b>LIVE L&D Sessions are held in your company's own Microsoft Teams, Google Meet, Zoom or Webex environment. For more information on how it works, click <a href="https://livelnd.com/how-it-works-live-l-and-d-explained/">here.</a></b></p>
                                             </div>
                                         </div>
 
