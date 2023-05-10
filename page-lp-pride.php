0bfc5a494598420bef67767abb5a59816ca77438
royboy31 <113940559+royboy31@users.noreply.github.com>
2023-05-10 12:51:32 +0200

    Update page-lp-pride.php

--- page-lp-pride.php
+++ page-lp-pride.php
@@ -501,10 +501,12 @@ $videosrc =  $server_name . '/wp-content/uploads/2022/09/pexels-artem-podrez-575
                                         <div class="kd-new-unique-text">
                                         <h5 class="text-center">Live L&D is the world’s first direct booking platform for expert speakers and facilitators</h5>
                                             <div class="unique-features">
+                                                <u>
                                                     <span>Instant Availability Check</span>
                                                     <span>Direct Booking</span>
                                                     <span>Book Now / Pay Later</span>
                                                     <span>Meet Experts Before Booking</span>
+                                                </u>
                                             </div>
                                         </div>
 										
