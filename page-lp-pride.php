e00c1f276430c93d53064264c12dfa5f24d13abe
royboy31 <113940559+royboy31@users.noreply.github.com>
2023-05-10 09:56:29 +0200

    preloader pride layout

--- page-lp-pride.php
+++ page-lp-pride.php
@@ -91,12 +91,9 @@ get_header();
 }
 
     #loading-text {
-       /* font-size: 34px !important;
-        font-weight: 700 !important;*/
         text-align: center !important;
         white-space: nowrap;
         letter-spacing: 0.15em !important;
-        color: white;
         position: relative;
         font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
         /* Change position to relative */
