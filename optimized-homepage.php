dee29084dc86266730f660401da541ce8c9f8086
kamindu <kamindu@itone.biz>
2023-05-12 16:15:54 +0530

    echo service again

--- optimized-homepage.php
+++ optimized-homepage.php
@@ -12,18 +12,24 @@ $tbprefix  = trim($tbprefix);
 $categoriesSql = "SELECT * FROM $tbprefix" . "amelia_categories GROUP BY `id` ORDER BY `position`;";
 $catResults = $wpdb->get_results($categoriesSql);
 
+// get excluded categories
+$exclude_cat_id = array();
+if (ot_get_option('exclude_category_id_s')) {
+    $exclude_cat_id = explode(',', ot_get_option('exclude_category_id_s'));
+}
+
 // get all service data
 $all_sql = "SELECT u.* , s.* FROM " . $tbprefix . "amelia_services s inner join " . $tbprefix . "amelia_providers_to_services p inner join " . $tbprefix . "amelia_users u on s.id=p.serviceId and p.userId=u.id where s.status  = 'visible' ORDER BY s.position ASC";
 print_r($all_sql);
 
 $all_service_data =  $wpdb->get_results($all_sql);
 
-// foreach($all_service_data as $single_service_data){
-//     echo '<pre>';
-//     print_r($all_service_data);
-//     echo '</pre>';
+foreach($all_service_data as $single_service_data){
+    echo '<pre>';
+    print_r($all_service_data);
+    echo '</pre>';
 
-// }
+}
 
 foreach ($catResults as $category) {
     print_r($category);
