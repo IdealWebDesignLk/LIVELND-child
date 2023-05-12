efd71c9d54c7542bf0e24e9a211343c5ee484e9d
kamindu <kamindu@itone.biz>
2023-05-12 16:38:14 +0530

    echo cat results

--- optimized-homepage.php
+++ optimized-homepage.php
@@ -8,10 +8,6 @@ global $wpdb;
 $tbprefix = $wpdb->prefix;
 $tbprefix  = trim($tbprefix);
 
-// get categories
-$categoriesSql = "SELECT * FROM $tbprefix" . "amelia_categories GROUP BY `id` ORDER BY `position`;";
-$catResults = $wpdb->get_results($categoriesSql);
-
 // get excluded categories
 $exclude_cat_id = array();
 if (ot_get_option('exclude_category_id_s')) {
@@ -20,22 +16,30 @@ if (ot_get_option('exclude_category_id_s')) {
 
 $exclude_cat_id_string = implode("' ,'", $exclude_cat_id);
 
+// get categories
+$categoriesSql = "SELECT * FROM $tbprefix" . "amelia_categories GROUP BY `id` WHERE id NOT IN('$exclude_cat_id_string') ORDER BY `position`;";
+$catResults = $wpdb->get_results($categoriesSql);
+
+forEach($catResults as $catresult){
+    echo $catresult;
+}
+
 // get all service data
 $all_sql = "SELECT u.* , s.* FROM " . $tbprefix . "amelia_services s inner join " . $tbprefix . "amelia_providers_to_services p inner join " . $tbprefix . "amelia_users u on s.id=p.serviceId and p.userId=u.id where s.status  = 'visible' and s.categoryId NOT IN('$exclude_cat_id_string') ORDER BY s.position ASC";
 print_r($all_sql);
 
-$all_service_data =  $wpdb->get_results($all_sql);
+// $all_service_data =  $wpdb->get_results($all_sql);
 
-foreach($all_service_data as $single_service_data){
-    echo '<pre>';
-    print_r($all_service_data);
-    echo '</pre>';
+// foreach($all_service_data as $single_service_data){
+//     echo '<pre>';
+//     print_r($all_service_data);
+//     echo '</pre>';
 
-}
+// }
 
-foreach ($catResults as $category) {
-    print_r($category);
-}
+// foreach ($catResults as $category) {
+//     print_r($category);
+// }
 
 
 
