9a2eba27b0693af5e33cf4c10fd903536aae20d1
kamindu <kamindu@itone.biz>
2023-05-12 11:00:27 +0530

    testing new sql query

--- optimized-homepage.php
+++ optimized-homepage.php
@@ -2,6 +2,23 @@
 <?php
 get_header();
 
+
+// iniiate wpdb
+global $wpdb;
+$tbprefix = $wpdb->prefix;
+$tbprefix  = trim($tbprefix);
+
+// get categories
+$categoriesSql = "SELECT * FROM $tbprefix" . "amelia_categories GROUP BY `id` ORDER BY `position`;";
+$catResults = $wpdb->get_results($categoriesSql);
+
+// get all service data
+$all_sql = "SELECT u.* , s.* FROM " . $tbprefix . "amelia_services s inner join " . $tbprefix . "amelia_providers_to_services p inner join " . $tbprefix . "amelia_users u on s.id=p.serviceId and p.userId=u.id where s.status  = 'visible' ORDER BY s.position ASC";
+print_r($all_sql);
+
+$employees =  $wpdb->get_results();
+
+
 ?>
 
 
