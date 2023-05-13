92010fc93eccda95d68d6736fce701f8292835b1
kamindu <kamindu@itone.biz>
2023-05-13 17:19:08 +0530

    echo cat result

--- optimized-homepage.php
+++ optimized-homepage.php
@@ -378,7 +378,7 @@ $employee =  $wpdb->get_results("SELECT $tbprefix" . "amelia_users.* FROM " . $t
 
         foreach ($catResults as $catresult) {
             echo '<pre>';
-            print_r(intval($catResult->id));
+            print_r($catresult);
             echo '</pre>';
 
             print_r($exclude_cat_id);
