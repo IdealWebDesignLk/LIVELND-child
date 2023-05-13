5be4d0faab2d79c61a23898f806f0b4df3e8094c
kamindu <kamindu@itone.biz>
2023-05-13 17:17:51 +0530

    print category ids

--- optimized-homepage.php
+++ optimized-homepage.php
@@ -377,8 +377,13 @@ $employee =  $wpdb->get_results("SELECT $tbprefix" . "amelia_users.* FROM " . $t
         }
 
         foreach ($catResults as $catresult) {
-            // print_r(intval($catResult->id));
-            // print_r($exclude_cat_id);
+            echo '<pre>';
+            print_r(intval($catResult->id));
+            echo '</pre>';
+
+            print_r($exclude_cat_id);
+
+
             if (!in_array(intval($catResult->id), $exclude_cat_id)) {
 
                 print_r(intval($catResult->id));
