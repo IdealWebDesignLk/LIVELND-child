3b75688f2c1891aed258810f6395dcc5ebbea32f
kamindu <kamindu@itone.biz>
2023-05-21 21:57:27 +0530

    adding youtbe video api to main video

--- assets/js/homepage-loader.js
+++ assets/js/homepage-loader.js
@@ -1,4 +1,33 @@
 // setting up header video
 window.addEventListener('load',()=>{
     document.getElementById('kd-main-youtube-video').src="https://www.youtube.com/embed/X0N22PMdF1U?enablejsapi=1&rel=0&start=18&mute=1&autoplay=1&modestbranding=1"
+
+
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    var player;
+
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player('kd-main-youtube-video', {
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    }
+
+    function onPlayerReady() {
+        console.log("hey Im ready");
+        //do whatever you want here. Like, player.playVideo();
+    }
+
+    function onPlayerStateChange() {
+        console.log("my state changed");
+    }
+
+    let videoStarted = 0
+
 })
