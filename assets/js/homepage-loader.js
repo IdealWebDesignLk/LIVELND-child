df3ac9cbcc0f3d2d39d843986f07289525b656da
kamindu <kamindu@itone.biz>
2023-05-21 21:50:45 +0530

    fixing js error

--- assets/js/homepage-loader.js
+++ assets/js/homepage-loader.js
@@ -1,4 +1,4 @@
 // setting up header video
-window.addeventListener('load',()=>{
+window.addEventListener('load',()=>{
     document.getElementById('kd-main-youtube-video').src="https://www.youtube.com/embed/X0N22PMdF1U?enablejsapi=1&rel=0&start=18&mute=1&autoplay=1&modestbranding=1"
 })
