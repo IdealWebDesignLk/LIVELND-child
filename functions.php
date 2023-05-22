4a7bfa2493958ce1531fdee30cf814e99fa0f4e0
kamindu <kamindu@itone.biz>
2023-05-22 16:35:13 +0530

    remove js files to check speed

--- functions.php
+++ functions.php
@@ -973,8 +973,8 @@ if (!function_exists('child_theme_configurator_css')) :
 		// new homepage assets
 		wp_enqueue_style('kd-new-homepage-css', get_stylesheet_directory_uri() . '/assets/css/optimized-homepge.css' , array() , '004' , 'all');
 		wp_enqueue_script('kd-main-video-js', get_stylesheet_directory_uri() . '/assets/js/homepage-main-video.js', array(), '', true);
-		wp_enqueue_script('kd-video-carousel-js', get_stylesheet_directory_uri() . '/assets/js/homepage-carousel-video-loader.js', array(), '', true);
-		wp_enqueue_script('kd-new-home-loaders', get_stylesheet_directory_uri() . '/assets/js/homepage-loader.js', array(), '20', true);
+		// wp_enqueue_script('kd-video-carousel-js', get_stylesheet_directory_uri() . '/assets/js/homepage-carousel-video-loader.js', array(), '', true);
+		// wp_enqueue_script('kd-new-home-loaders', get_stylesheet_directory_uri() . '/assets/js/homepage-loader.js', array(), '20', true);
 	}
 endif;
 add_action('wp_enqueue_scripts', 'child_theme_configurator_css', 100);
