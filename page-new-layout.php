180de137f98be3d668a1fe82a26b077b3009525c
kamindu <kamindu@itone.biz>
2023-06-02 17:06:40 +0530

    checking

--- page-new-layout.php
+++ page-new-layout.php
@@ -139,6 +139,8 @@ $imgurl =  ot_get_option('top_section_background_image');
     print_r($categoriesSql);
 
     $catResults = $wpdb->get_results($categoriesSql);
+
+    print_r($catResults);
     $exclude_cat_id = array();
     if (ot_get_option('exclude_category_id_s')) {
         $exclude_cat_id = explode(',', ot_get_option('exclude_category_id_s'));
