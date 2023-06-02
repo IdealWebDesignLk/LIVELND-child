ace2f18254f06894a053682050cd5b475fbdb89b
kamindu <kamindu@itone.biz>
2023-06-02 16:27:41 +0530

    basic header html

--- page-new-layout.php
+++ page-new-layout.php
@@ -8,8 +8,42 @@ $imgurl =  ot_get_option('top_section_background_image');
 ?>
 <div class="new-homepage-wrapper">
     <!-- hero area -->
-    <div class="hero-area-wrapper">
-        <img src="<?php echo $imgurl; ?>" alt="" srcset="">
+    <div class="hero-area-wrapper" style="background: url('<?php echo $imgurl; ?>');">
+
+        <div class="row">
+            <div class="col-md-7">
+                <h1>Book the best <br>
+                    [topic] speakers for <br>
+                    Your team</h1>
+                <p>Live, Online & exclusively for your company.</p>
+
+            </div>
+            <div class="col-md-5">
+                <p class="main-session-info">
+                    Like Anotonia Forster,
+                    'LGBTQ' + it's Natural
+                </p>
+                <div class="start-rating-wrapper">
+                    <div class="ratings">
+                        <ul>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                        </ul>
+                    </div>
+                    <div class="speaker">
+                        By Delottie HR
+                    </div>
+                    <div class="more-about-speaker">
+                        <a href="#">More About This Speaker</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
     </div>
 </div>
 
